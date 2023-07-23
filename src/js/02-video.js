de743dcbadfddd4472336e4273d1f53a1901baf6
VictoriaTorry <k.victoria.torry@gmail.com>
2023-07-23 11:30:14 +0300

    complete second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timekey = 'videoplayer-current-time';
+
+function durationSetlocalstorage({ seconds }) {
+  localStorage.setItem(timekey, seconds);
+}
+
+window.addEventListener('load', newStart);
+
+player.on('timeupdate', Throttle(durationSetlocalstorage, 1000));
+
+function newStart() {
+  if (!localStorage.getItem(timekey)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(timekey) ?? 0);
+}
